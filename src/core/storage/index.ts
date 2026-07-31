a10e4cdd780221155d59f0d6f3dc51a2bf7e390c
Zelijah <zeffabian@gmail.com>
2026-07-31 21:03:34 +0800

    fix: reorganize exports and add missing type exports from majik-key

--- src/core/storage/index.ts
+++ src/core/storage/index.ts
@@ -4,14 +4,30 @@ export * from "./sql-db-manager";
 export * from "./storage-adapter";
 export * from "./idb-adapter";
 
-
 export * from "./keystore/adapter-idb";
 export * from "./keystore/adapter-sql";
 export * from "./keystore/adapter-memory";
 export type * from "./keystore/_types";
 
-
 export * from "./client-state/adapter-idb";
 export * from "./client-state/adapter-sql";
 export * from "./client-state/adapter-memory";
 export type * from "./client-state/_types";
+
+export type {
+  MnemonicJSON,
+  MajikKeyAddress,
+  MajikKeyJSON,
+  MajikKeyMetadata,
+  MajikKeyIdentity,
+  MajikKeyFingerprint,
+  MajikKeyConstructorOptions,
+  MajikKeyDangerousJSON,
+  MajikKeySolanaNamespace,
+  MajikKeyBitcoinNamespace,
+  MajikKeyWeb3Namespace,
+  BitcoinDerivationOptions,
+  BitcoinKeypairMaterial,
+  SerializedIdentity,
+  SolanaKeypairMaterial,
+} from "@majikah/majik-key";
